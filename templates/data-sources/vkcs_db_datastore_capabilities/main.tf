b6194c4aaf16a651eacece48e01581ddca07c54d
panic <speshilovmax@gmail.com>
2023-04-06 19:37:22 +0300

    Update examples for datastore params and caps

---
+++ templates/data-sources/vkcs_db_datastore_capabilities/main.tf
@@ -0,0 +1,9 @@
+data "vkcs_db_datastore_capabilities" "postgres_caps" {
+	datastore_name = data.vkcs_db_datastore.postgres
+	datastore_version_id = local.pg_v14_version_id
+}
+
+output "postgresql_capabilities" {
+	value = data.vkcs_db_datastore_capabilities.postgres_caps.capabilities
+	description = "Available capabilities of the latest version of PostgreSQL datastore."
+}
