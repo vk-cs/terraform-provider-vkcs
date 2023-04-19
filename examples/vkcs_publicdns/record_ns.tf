d33057745fd22eb31ff74ed7ec63f892acd9ff67
panic <speshilovmax@gmail.com>
2023-04-19 15:16:17 +0300

    Add examples for dns records of all types

---
+++ examples/vkcs_publicdns/record_ns.tf
@@ -0,0 +1,7 @@
+resource "vkcs_publicdns_record" "ns" {
+  zone_id = vkcs_publicdns_zone.zone.id
+  type = "NS"
+  name = "@"
+  content = "ns1.example.com"
+  ttl = 86400
+}
---
+++ examples/vkcs_publicdns/record_txt.tf
@@ -0,0 +1,7 @@
+resource "vkcs_publicdns_record" "txt" {
+  zone_id = vkcs_publicdns_zone.zone.id
+  type = "TXT"
+  name = "@"
+  content = "Text example"
+  ttl = 86400
+}
