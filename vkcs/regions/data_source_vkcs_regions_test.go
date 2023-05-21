563fae0a892cc7d00e929747c29b429ff1dc090b
panic <speshilovmax@gmail.com>
2023-05-21 22:11:12 +0300

    Update regions tests

--- vkcs/regions/data_source_vkcs_regions_test.go
+++ vkcs/regions/data_source_vkcs_regions_test.go
@@ -10,64 +10,20 @@ import (
 	"github.com/vk-cs/terraform-provider-vkcs/vkcs/internal/acctest"
 )
 
-func TestAccDataSourceVkcsRegions(t *testing.T) {
-	tests := map[string]struct {
-		name     string
-		testCase resource.TestCase
-	}{
-		"no filter": {
-			name: "data.vkcs_regions.empty",
-			testCase: resource.TestCase{
-				ProviderFactories: acctest.AccTestProviders,
-				Steps: []resource.TestStep{
-					{
-						Config: testAccdataSourceVkcsRegionsConfigEmpty(),
-						Check: resource.ComposeTestCheckFunc(
-							testAccdataSourceVkcsRegionsCheck("data.vkcs_regions.empty"),
-						),
-					},
-				},
+func TestAccDataSourceVkcsRegions_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { acctest.AccTestPreCheck(t) },
+		ProviderFactories: acctest.AccTestProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRegionsDataSourceConfigBasic,
+				Check:  testAccCheckRegionsDataSource("data.vkcs_regions.regions"),
 			},
 		},
-		"with parent id": {
-			name: "data.vkcs_regions.parent_id",
-			testCase: resource.TestCase{
-				ProviderFactories: acctest.AccTestProviders,
-				Steps: []resource.TestStep{
-					{
-						Config: testAccdataSourceVkcsRegionsConfigParentID(),
-						Check: resource.ComposeTestCheckFunc(
-							testAccdataSourceVkcsRegionsCheck("data.vkcs_regions.parent_id"),
-						),
-					},
-				},
-			},
-		},
-	}
-
-	for name := range tests {
-		tt := tests[name]
-		t.Run(name, func(t *testing.T) {
-			resource.ParallelTest(t, tt.testCase)
-		})
-	}
+	})
 }
 
-func testAccdataSourceVkcsRegionsConfigEmpty() string {
-	return `
-data "vkcs_regions" "empty" {}
-`
-}
-
-func testAccdataSourceVkcsRegionsConfigParentID() string {
-	return `
-data "vkcs_regions" "parent_id" {
-	parent_region_id=""
-}
-`
-}
-
-func testAccdataSourceVkcsRegionsCheck(resourceName string) resource.TestCheckFunc {
+func testAccCheckRegionsDataSource(resourceName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[resourceName]
 
@@ -94,3 +50,7 @@ func testAccdataSourceVkcsRegionsCheck(resourceName string) resource.TestCheckFu
 		return nil
 	}
 }
+
+const testAccRegionsDataSourceConfigBasic = `
+data "vkcs_regions" "regions" {}
+`
