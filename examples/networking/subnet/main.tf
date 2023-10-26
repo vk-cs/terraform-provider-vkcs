81433229c4c9f2d5ad6daebafc2d574f1a982e34
Николай Калужин <n.kaluzhin@vk.team>
2023-10-26 15:43:04 +0300

    template subnet

---
+++ examples/networking/subnet/main.tf
@@ -0,0 +1,5 @@
+resource "vkcs_networking_subnet" "app" {
+  name       = "app-tf-example"
+  network_id = vkcs_networking_network.app.id
+  cidr       = "192.168.199.0/24"
+}
--- templates/network/resources/vkcs_networking_subnet/main.tf
+++
@@ -1,9 +0,0 @@
-resource "vkcs_networking_network" "network_1" {
-  name           = "tf_test_network"
-  admin_state_up = "true"
-}
-
-resource "vkcs_networking_subnet" "subnet_1" {
-  network_id = "${vkcs_networking_network.network_1.id}"
-  cidr       = "192.168.199.0/24"
-}
