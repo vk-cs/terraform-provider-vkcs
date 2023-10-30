996f45c427afed8c82ade20f70fc345f9a854260
Николай Калужин <n.kaluzhin@vk.team>
2023-10-30 18:15:03 +0300

    template router_interface

---
+++ examples/networking/router_interface/base-network.tf
@@ -0,0 +1,1 @@
+../network/main.tf
---
+++ examples/networking/router_interface/main.tf
@@ -0,0 +1,4 @@
+resource "vkcs_networking_router_interface" "app" {
+  router_id = vkcs_networking_router.router.id
+  subnet_id = vkcs_networking_subnet.app.id
+}
