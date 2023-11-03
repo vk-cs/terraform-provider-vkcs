ac4c1669dd38b3ead31ffea32230ff0de1c50875
Николай <n.kaluzhin@vk.team>
2023-11-03 19:07:35 +0300

    resource floatingip associate

---
+++ examples/networking/floatingip_associate/base-port.tf
@@ -0,0 +1,1 @@
+../port/main.tf
--- templates/networking/resources/vkcs_networking_floatingip_associate/main.tf
+++
@@ -1,8 +0,0 @@
-resource "vkcs_networking_port" "port_1" {
-  network_id = "a5bbd213-e1d3-49b6-aed1-9df60ea94b9a"
-}
-
-resource "vkcs_networking_floatingip_associate" "fip_1" {
-  floating_ip = "1.2.3.4"
-  port_id     = "${vkcs_networking_port.port_1.id}"
-}
