730964f51e0c14dcd853cbe773d2126ebf83575d
Николай <n.kaluzhin@vk.team>
2023-11-14 13:55:12 +0300

    datasource secgroup

---
+++ examples/firewall/networking_secgroup/main-datasource.tf
@@ -0,0 +1,6 @@
+data "vkcs_networking_secgroup" "etcd" {
+  name       = "etcd-tf-example"
+  # This is unnecessary in real life.
+  # This is required here to let the example work with secgroup resource example. 
+  depends_on = [vkcs_networking_secgroup.etcd]
+}
