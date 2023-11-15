605933df663ee3b907c1d25460f71e5e90bd4c31
Николай <n.kaluzhin@vk.team>
2023-11-15 17:47:07 +0300

    datasources for templates and later usage of it

--- templates/kubernetes/data-sources/vkcs_kubernetes_clustertemplate/with_version/main.tf
+++
@@ -1,7 +0,0 @@
-data "vkcs_kubernetes_clustertemplate" "example_template_by_version" {
-  version = "1.20.4"
-}
-
-output "example_template_id" {
-  value = "${data.vkcs_kubernetes_clustertemplate.example_template_by_version.id}"
-}
