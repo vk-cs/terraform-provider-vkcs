8ae857446334aa6e9157b72f53c4fe6ed0e83e17
panic <speshilovmax@gmail.com>
2023-11-17 15:22:43 +0300

    Fix inconsistent result error when creating  vkcs_kubernetes_addon

--- vkcs/kubernetes/data_source_addon_test.go
+++ vkcs/kubernetes/data_source_addon_test.go
@@ -5,17 +5,27 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/vk-cs/terraform-provider-vkcs/vkcs/internal/acctest"
+	"github.com/vk-cs/terraform-provider-vkcs/vkcs/internal/services/containerinfra/v1/clusters"
 )
 
 func TestAccKubernetesAddonDataSource_basic_big(t *testing.T) {
+	var cluster clusters.Cluster
+	baseConfig := acctest.AccTestRenderConfig(testAccKubernetesAddonClusterBase,
+		map[string]string{"TestAccKubernetesAddonNetworkingBase": testAccKubernetesAddonNetworkingBase})
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV6ProviderFactories: acctest.AccTestProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
+				Config: baseConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckKubernetesClusterExists("vkcs_kubernetes_cluster.cluster", &cluster),
+				),
+			},
+			{
 				Config: acctest.AccTestRenderConfig(testAccKubernetesAddonDataSourceBasic, map[string]string{
-					"TestAccKubernetesAddonNetworkingBase": testAccKubernetesAddonNetworkingBase,
-					"TestAccKubernetesAddonClusterBase":    testAccKubernetesAddonClusterBase,
+					"TestAccKubernetesAddonClusterBase": baseConfig,
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrPair("data.vkcs_kubernetes_addon.ingress-nginx", "cluster_id", "vkcs_kubernetes_cluster.cluster", "id"),
@@ -27,7 +37,6 @@ func TestAccKubernetesAddonDataSource_basic_big(t *testing.T) {
 }
 
 const testAccKubernetesAddonDataSourceBasic = `
-{{ .TestAccKubernetesAddonNetworkingBase }}
 {{ .TestAccKubernetesAddonClusterBase }}
 
 data "vkcs_kubernetes_addon" "ingress-nginx" {
--- vkcs/kubernetes/data_source_addons_test.go
+++ vkcs/kubernetes/data_source_addons_test.go
@@ -5,17 +5,27 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/vk-cs/terraform-provider-vkcs/vkcs/internal/acctest"
+	"github.com/vk-cs/terraform-provider-vkcs/vkcs/internal/services/containerinfra/v1/clusters"
 )
 
 func TestAccKubernetesAddonsDataSource_basic_big(t *testing.T) {
+	var cluster clusters.Cluster
+	baseConfig := acctest.AccTestRenderConfig(testAccKubernetesAddonClusterBase,
+		map[string]string{"TestAccKubernetesAddonNetworkingBase": testAccKubernetesAddonNetworkingBase})
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV6ProviderFactories: acctest.AccTestProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
+				Config: baseConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckKubernetesClusterExists("vkcs_kubernetes_cluster.cluster", &cluster),
+				),
+			},
+			{
 				Config: acctest.AccTestRenderConfig(testAccKubernetesAddonsDataSourceBasic, map[string]string{
-					"TestAccKubernetesAddonNetworkingBase": testAccKubernetesAddonNetworkingBase,
-					"TestAccKubernetesAddonClusterBase":    testAccKubernetesAddonClusterBase,
+					"TestAccKubernetesAddonClusterBase": baseConfig,
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrPair("data.vkcs_kubernetes_addons.cluster-addons", "cluster_id", "vkcs_kubernetes_cluster.cluster", "id"),
@@ -26,7 +36,6 @@ func TestAccKubernetesAddonsDataSource_basic_big(t *testing.T) {
 }
 
 const testAccKubernetesAddonsDataSourceBasic = `
-{{ .TestAccKubernetesAddonNetworkingBase }}
 {{ .TestAccKubernetesAddonClusterBase }}
 
 data "vkcs_kubernetes_addons" "cluster-addons" {
