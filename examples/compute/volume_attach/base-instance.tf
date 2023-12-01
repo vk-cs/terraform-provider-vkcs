710900facd6d02af5fbbef46acef036cdec626b5
Николай <n.kaluzhin@vk.team>
2023-12-01 11:50:18 +0300

    resource_volume_attach

---
+++ examples/compute/volume_attach/base-instance.tf
@@ -0,0 +1,1 @@
+../instance/basic/main.tf
---
+++ examples/compute/volume_attach/main.tf
@@ -0,0 +1,4 @@
+resource "vkcs_compute_volume_attach" "data" {
+  instance_id = vkcs_compute_instance.basic.id
+  volume_id   = vkcs_blockstorage_volume.volume.id
+}
