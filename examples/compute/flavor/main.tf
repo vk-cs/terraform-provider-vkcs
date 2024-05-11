3c89afd3ab241406bf81419b33486f8a8f8569bc
morozovalekseywot <morozovalekseywot@gmail.com>
2024-05-11 17:02:23 +0300

    code review fixes

--- examples/compute/flavor/main.tf
+++ examples/compute/flavor/main.tf
@@ -1,6 +1,7 @@
 data "vkcs_compute_flavor" "basic" {
-  vcpus   = 2
-  ram     = 2048
+  vcpus = 1
+  ram   = 1024
+  # specify cpu_generation to distinguish between several flavors with the same CPU and RAM 
   extra_specs = {
     "mcs:cpu_generation" : "cascadelake-v1"
   }
