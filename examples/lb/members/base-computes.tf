c2e25c4736a454ac27450bd402a2818ce77bf9a2
morozovalekseywot <morozovalekseywot@gmail.com>
2024-05-13 16:35:06 +0300

    fix tf

--- examples/lb/members/base-computes.tf
+++ examples/lb/members/base-computes.tf
@@ -1,7 +1,7 @@
 resource "vkcs_compute_instance" "front_worker" {
-  count              = 2
-  name               = "front-worker-${count.index}-tf-example"
-  flavor_id          = data.vkcs_compute_flavor.basic.id
+  count     = 2
+  name      = "front-worker-${count.index}-tf-example"
+  flavor_id = data.vkcs_compute_flavor.basic.id
   security_group_ids = [
     data.vkcs_networking_secgroup.default_secgroup.id,
     vkcs_networking_secgroup.admin.id,
