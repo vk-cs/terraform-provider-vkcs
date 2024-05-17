2bb4471ce0818874504949e12a2d6c3e7a0c2cfb
morozovalekseywot <morozovalekseywot@gmail.com>
2024-05-17 13:47:06 +0300

    fix basic flavor

--- examples/compute/flavor/main.tf
+++ examples/compute/flavor/main.tf
@@ -1,8 +1,5 @@
 data "vkcs_compute_flavor" "basic" {
   vcpus = 1
   ram   = 1024
-  # specify cpu_generation to distinguish between several flavors with the same CPU and RAM 
-  extra_specs = {
-    "mcs:cpu_generation" : "cascadelake-v1"
-  }
+  disk = 10
 }
