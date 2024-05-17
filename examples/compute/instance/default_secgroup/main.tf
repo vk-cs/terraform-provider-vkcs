a72121215a82191ef1cba5c0afe1164f19cb2fe5
morozovalekseywot <morozovalekseywot@gmail.com>
2024-05-17 15:54:38 +0300

    fix compute instances

--- examples/compute/instance/default_secgroup/main.tf
+++ examples/compute/instance/default_secgroup/main.tf
@@ -1,6 +1,17 @@
 resource "vkcs_compute_instance" "front_worker" {
   name      = "front-worker-tf-example"
   flavor_id = data.vkcs_compute_flavor.basic.id
+  
+  block_device {
+    source_type      = "image"
+    uuid             = data.vkcs_images_image.debian.id
+    destination_type = "volume"
+    volume_size      = 10
+    # Must be set to delete volume after instance deletion
+    # Otherwise you get "orphaned" volume with terraform
+    delete_on_termination = true
+  }
+
   security_group_ids = [
     data.vkcs_networking_secgroup.default_secgroup.id,
     vkcs_networking_secgroup.admin.id,
--- examples/lb/members/base-computes.tf
+++ examples/lb/members/base-computes.tf
@@ -2,11 +2,21 @@ resource "vkcs_compute_instance" "front_worker" {
   count     = 2
   name      = "front-worker-${count.index}-tf-example"
   flavor_id = data.vkcs_compute_flavor.basic.id
+
+  block_device {
+    source_type      = "image"
+    uuid             = data.vkcs_images_image.debian.id
+    destination_type = "volume"
+    volume_size      = 10
+    # Must be set to delete volume after instance deletion
+    # Otherwise you get "orphaned" volume with terraform
+    delete_on_termination = true
+  }
+
   security_group_ids = [
     vkcs_networking_secgroup.admin.id,
     vkcs_networking_secgroup.http.id
   ]
-  image_id = data.vkcs_images_image.debian.id
 
   network {
     uuid = vkcs_networking_network.app.id
