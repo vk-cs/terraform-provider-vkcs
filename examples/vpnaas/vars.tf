0791ae4e65fa8e441e7ed9c04ad7ede18b5294e3
morozovalekseywot <morozovalekseywot@gmail.com>
2024-05-28 14:34:47 +0300

    Add example for sprut-neutron vpn tunnel

---
+++ examples/vpnaas/vars.tf
@@ -0,0 +1,9 @@
+locals {
+  sdn_sprut               = "sprut"
+  sdn_neutron             = "neutron"
+  phase1_negotiation_mode = "main"
+  pfs                     = "group5"
+  auth_algorithm          = "sha256"
+  encryption_algorithm    = "aes-128"
+  psk_key                 = "any_psk_key"
+}
