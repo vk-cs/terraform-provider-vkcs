b7aab19401e7a4904a8c5f04ee0fd6a4b60679e7
morozovalekseywot <morozovalekseywot@gmail.com>
2024-05-31 16:20:38 +0300

    fix

--- examples/vpnaas/service/main.tf
+++ examples/vpnaas/service/main.tf
@@ -2,6 +2,6 @@ resource "vkcs_vpnaas_service" "vpn_to_datacenter" {
   name      = "vpn-tf-example"
 
   # See the argument description and check vkcs_networks_sdn datasource output to figure out
-  # what type of router you should use in certain case (router or dc_router)
+  # what type of router you should use in certain case (vkcs_networking_router or vkcs_dc_router)
   router_id = vkcs_networking_router.router.id
 }
