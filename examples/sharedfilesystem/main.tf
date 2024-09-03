9900955117cc9d18e5c946d7bbf8874b7fae63b9
morozovalekseywot <morozovalekseywot@gmail.com>
2024-09-03 12:50:13 +0300

    fix examples

--- examples/sharedfilesystem/main.tf
+++ examples/sharedfilesystem/main.tf
@@ -1,7 +1,7 @@
 resource "vkcs_sharedfilesystem_sharenetwork" "sharenetwork" {
   name                = "test_sharenetwork"
-  neutron_net_id      = "${vkcs_networking_network.sfs.id}"
-  neutron_subnet_id   = "${vkcs_networking_subnet.sfs.id}"
+  neutron_net_id      = vkcs_networking_network.sfs.id
+  neutron_subnet_id   = vkcs_networking_subnet.sfs.id
 }
 
 resource "vkcs_sharedfilesystem_share" "share" {
@@ -10,18 +10,18 @@ resource "vkcs_sharedfilesystem_share" "share" {
   share_proto      = "NFS"
   share_type       = "default_share_type"
   size             = 1
-  share_network_id = "${vkcs_sharedfilesystem_sharenetwork.sharenetwork.id}"
+  share_network_id = vkcs_sharedfilesystem_sharenetwork.sharenetwork.id
 }
 
 resource "vkcs_sharedfilesystem_share_access" "share_access_1" {
-  share_id     = "${vkcs_sharedfilesystem_share.share.id}"
+  share_id     = vkcs_sharedfilesystem_share.share.id
   access_type  = "ip"
   access_to    = "192.168.199.10"
   access_level = "rw"
 }
 
 resource "vkcs_sharedfilesystem_share_access" "share_access_2" {
-  share_id     = "${vkcs_sharedfilesystem_share.share.id}"
+  share_id     = vkcs_sharedfilesystem_share.share.id
   access_type  = "ip"
   access_to    = "192.168.199.11"
   access_level = "rw"
