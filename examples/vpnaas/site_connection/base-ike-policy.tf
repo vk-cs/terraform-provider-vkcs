4abf81f28e30def9f5fcac80efeb49b9657f011c
Stanislav Chirevko <schirevko@gmail.com>
2025-02-26 10:32:35 +0300

    Switch tests from neutron to sprut

--- examples/vpnaas/site_connection/base-ike-policy.tf
+++ examples/vpnaas/site_connection/base-ike-policy.tf
@@ -1,1 +1,7 @@
-../ike_policy/main.tf
+resource "vkcs_vpnaas_ike_policy" "data_center" {
+  name           = "key-policy-tf-example"
+  description    = "Policy that restricts remote working users to connect to our data ceneter over VPN"
+  auth_algorithm = "sha256"
+
+  sdn = "neutron"
+}
