9eac527408d2866d9e4ccdd208d148251846a9f8
Stanislav Chirevko <schirevko@gmail.com>
2025-07-25 12:40:33 +0300

    Remove go:generate cdn generation

--- generate.go
+++
@@ -1,4 +0,0 @@
-package main
-
-//go:generate tfplugingen-framework generate resources --input vkcs/cdn/spec.json --output vkcs/cdn
-//go:generate tfplugingen-framework generate data-sources --input vkcs/cdn/spec.json --output vkcs/cdn
