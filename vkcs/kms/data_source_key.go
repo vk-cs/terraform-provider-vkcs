3d0fd5ec5dd6e003eb4b2566e77b05be27823d60
olegdayo <ol-sidorenkov@mail.ru>
2026-09-21 09:22:48 +0300

    kms init

---
+++ vkcs/kms/data_source_key.go
@@ -0,0 +1,7 @@
+package kms
+
+import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+func DataSourceKey() *schema.Resource {
+	return &schema.Resource{}
+}
---
+++ vkcs/kms/data_source_key_encrypt.go
@@ -0,0 +1,7 @@
+package kms
+
+import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+func DataSourceKeyEncrypt() *schema.Resource {
+	return &schema.Resource{}
+}
